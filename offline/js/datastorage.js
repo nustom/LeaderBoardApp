445df602b165920796f0e22a69749cf4fe11da9b
nustom <tom@nustechnology.com>
2020-02-03 17:48:25 +0700

    Update: Refresh dashboard periodically (1second)

--- offline/js/datastorage.js
+++ offline/js/datastorage.js
@@ -1,14 +1,29 @@
 //Wrapper to handle data communicaion with localstorage
 class DataStorage {
   constructor(){
-    this.key = "leaderboard_data"
+    this.key = "leaderboard_data";
   }
 
   get(){
-    return JSON.parse(localStorage.getItem(this.key));
+    data = JSON.parse(localStorage.getItem(this.key));
+    data.forEach(function(item){
+      item.last_lap = parseFloat(item.last_lap).toFixed(2);
+    });
+
+    data.sort(function(a,b){
+      if (a.position > b.position) {
+        return 1;
+      }
+      if (b.position > a.position) {
+          return -1;
+      }
+      return 0;
+    });
+
+    return data;
   }
 
   set(data){
-    localStorage.setItem(this.key, JSON.stringify(data));
+    localStorage.setItem(this.key, data);
   }
 }
--- offline/js/leaderboard.js
+++ offline/js/leaderboard.js
@@ -2,14 +2,18 @@
 class LeaderBoard {
   constructor(){
     this.dataStorage = new DataStorage();
+    var theTemplateScript = document.getElementById('leaderboard-template').innerHTML;
+    this.theTemplate = Handlebars.compile(theTemplateScript);
+
   }
   refreshDashboard(){
-    setInterval(this.renderDashboard.bind(this), 1000)
+    setInterval(this.renderDashboard.bind(this), 1000);
     
   }
 
   renderDashboard(){
-    var data = this.dataStorage.get(data);
+    var data = this.dataStorage.get();
+    document.getElementById('leaderboard-wrapper').innerHTML = this.theTemplate({people: data});
   }
 
 }
